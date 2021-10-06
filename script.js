081ba43b7333915610fa9ad434a99f20a102ab8a
Kyana <92030162+KyanaAlter@users.noreply.github.com>
2021-10-06 19:28:19 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function startlog()
+        {
+            var inputan1 = document.getElementById('nama').value
+            + 
+            document.getElementById('katasandi').value
+            ;
+            var inputan2 = ('Muhammad Rizky Perdana') + ('0909');
+             if (inputan1 == inputan2){
+                alert('Login Sukses')
+             }
+             else {
+                 alert('Login Gagal');
+             };
+            }
+//     var inputan1 = document.getElementById('nama').value
+//      + 
+//     document.getElementById('katasandi').value
+//     ;
+//     var inputan2 = ('Muhammad Rizky Perdana') + ('0909');
+//     if (inputan1 == inputan2){
+//         var logakses = document.getElementById('akses').innerHTML ='*Login Sukses'
+//     }
+//     else {
+//         var logakses = document.getElementById('akses').innerHTML ='*Login Gagal';
+//     };
+// }
